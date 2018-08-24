5489accb5c08f5b52243723b4076a181a2626ee2
Miranda Jessie <mirandajessie@MacBook-Pro-27.local>
2018-08-24 10:21:10 -0500

    made changes to strings

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -16,14 +16,14 @@ repeat {
     //This is the random number that we have generated. We cast it from a UInt32 to an Int.
     let randomNumber = Int(arc4random_uniform(101))
     
-    print("Random number: \(randomNumber)")
+  
     
     //This is the number of guesses a user has to get it right.
     var numberOfGuesses = 5
     
     //Welcome the user and prompt them for input
-    print("Welcome to the guessing game!")
-    print("Please guess a number between 0 and 100.")
+    print("Welcome to the guessing game! You must be really bored if you are playing this. Anyways, have fun.")
+    print("Go ahead and guess a number between 0 and 100. Try to be smart about it.")
     var userGuess = Int(readLine()!)
     
     //If the user enters a value that can't be converted to an interger, userGuess will equal nil.
@@ -31,7 +31,7 @@ repeat {
     //Input Validation
     while userGuess == nil {
         //Prompt the user to enter a valid number
-        print("Wrong.Try using a number.")
+        print("How about using a number this time since this is a guessing game based around numbers...")
         //Get user input again
         userGuess = Int(readLine()!)
     }
@@ -44,17 +44,17 @@ repeat {
             //Remove a guess
             numberOfGuesses -= 1
             //Tell the user how many tries they have remaining
-            print("You have  \(numberOfGuesses) tries remaining.")
+            print("You have  \(numberOfGuesses) tries remaining. Try to make them count.")
             if userGuess! < randomNumber {
-                print("Try picking a hgher number.")
-            } else if userGuess! > randomNumber {
-                print("Try picking a lower number.")
+                print("Try picking a higher number this time but not too high ;).")
+            } else if userGuess! > randomNumber { 
+                print("Try picking a lower number but not too low ;).")
             }
             userGuess = Int(readLine()!)
             
             while userGuess == nil {
                 //Prompt the user to enter a valid number
-                print("How about you try using a number this time.")
+                print("How about using a number this time since this is a guessing game based around numbers...")
                 //Get user input again
                 userGuess = Int(readLine()!)
             }
@@ -63,12 +63,12 @@ repeat {
     //Check to see if the user is out of guesses and hasn't guessed the correct number.
     if numberOfGuesses == 1 && userGuess != randomNumber {
         //Tell the user they are out of tries, and prompt them on if they want to play again.
-        print("You ran out of tries. The correct answer was \(randomNumber).")
-        print("Do you want to play again? Y/N")
+        print("You ran out of tries. I told you to use them wisely.. The correct answer was \(randomNumber). Better luck next time.")
+        print("Do you want to try and redeem yourself or are you going to just give up? Y/N")
         var stillPlaying = readLine()!.uppercased()
         while stillPlaying != "Y" && stillPlaying != "N" {
-            print("Would you like to play agian? Y/N")
-            stillPlaying = readLine()!
+            print("Do you want to try and redeem yourself or are you going to just give up? Y/N")
+            stillPlaying = readLine()!.uppercased()
         }
         if stillPlaying == "N" {
             playingGame = false
@@ -79,12 +79,12 @@ repeat {
     
         //Check to see if the user guess is equal to the random number.
     if userGuess == randomNumber {
-            print("You guessed the number!")
-            print("Would you like to play again? Y/N")
+            print("You guessed the number! I guess you do have what it takes. Look at you..")
+            print("Would you like to play again and waste more of your time? Y/N")
             
             var stillPlaying = readLine()!.uppercased()
             while stillPlaying != "Y" && stillPlaying != "N" {
-                print("Would you like to play again?")
+                print("Would you like to play again and waste more of your time? Y/N?")
                 stillPlaying = readLine()!.uppercased()
                 
             }
@@ -93,3 +93,5 @@ repeat {
             }
         }
     } while playingGame == true
+
+
